8ab8cd77fba2f600b26d08c4135727c35cd5bbad
GhufranRifaldi <gufran.rivaldi@gmail.com>
2020-11-18 12:52:16 +0700

    deploy

---
+++ build/precache-manifest.496bff29f5db6a050b17d56c92363293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7734d7fa04df6af0158c066741ef9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2e1e1ab15ba8e3cc04",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "eb1a5087db8e6b3edeb0",
+    "url": "/static/css/main.63473b9c.chunk.css"
+  },
+  {
+    "revision": "9c2e1e1ab15ba8e3cc04",
+    "url": "/static/js/2.8bd2a859.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8bd2a859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1a5087db8e6b3edeb0",
+    "url": "/static/js/main.75447b2a.chunk.js"
+  },
+  {
+    "revision": "1488748ec381c9cb3705",
+    "url": "/static/js/runtime-main.7ade24de.js"
+  }
+]);
